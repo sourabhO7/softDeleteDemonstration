ddd7c55930cf4d2f71184f846d37f5742aed6d86
Sourabh Mukherjee <112678696+sourabhO7@users.noreply.github.com>
2025-04-01 21:03:51 +0530

    soft delete code repo is uploaded

---
+++ softDelete/src/main/java/com/app/softDelete/SoftDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.app.softDelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftDeleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftDeleteApplication.class, args);
+	}
+
+}
---
+++ softDelete/src/test/java/com/app/softDelete/SoftDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.softDelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftDeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
